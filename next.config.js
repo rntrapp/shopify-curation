3591bb5d8e37fd42d3fe93fbcff7f4fe71bd3139
aydin <aydin4ik@gmail.com>
2025-04-04 09:59:55 +1100

    fix: migrate Next.js config from TypeScript to JavaScript

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['rntrassemblylabelimages.blob.core.windows.net']
+  }
+};
+
+module.exports = nextConfig; 
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['rntrassemblylabelimages.blob.core.windows.net']
-  }
-};
-
-export default nextConfig;
